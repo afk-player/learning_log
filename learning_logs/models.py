e5150321b4b8a3d89c94d428096dd3257a86c40e
Zakhar Schebet <lovemebaby453@gmail.com>
2020-07-28 10:36:46 +0300

    add new migrations file

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -2,10 +2,28 @@ from django.db import models
 
 
 class Topic(models.Model):
-    """Topic, that user stydies"""
+    """Topic, that user studies"""
 
     text = models.CharField(max_length=200)
     data_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.text
+
+
+class Entry(models.Model):
+    """Entry for Topics"""
+
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        length = len(self.text[:])
+        if length >= 50:
+            return f'{self.text[:50]}...'
+        else:
+            return f'{self.text}'
