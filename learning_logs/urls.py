b805e4c78f66c2f09c4699dc647046c2195f2508
Zakhar Schebet <lovemebaby453@gmail.com>
2020-07-28 13:18:39 +0300

    add urls, templates for learning_logs app, add views function, start test homepage of the site

---
+++ learning_logs/urls.py
@@ -0,0 +1,11 @@
+"""Define URL schemas for learning_logs"""
+
+from django.urls import path
+
+from . import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+    # Homepage
+    path('', views.index, name='index'),
+]
