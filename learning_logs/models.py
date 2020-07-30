f22da8213781ce2878089df8ff60689564da6e1c
Zakhar Schebet <lovemebaby453@gmail.com>
2020-07-30 17:25:56 +0300

    add topic detail pages

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -5,7 +5,7 @@ class Topic(models.Model):
     """Topic, that user studies"""
 
     text = models.CharField(max_length=200)
-    data_added = models.DateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.text
--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,4 +8,8 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Homepage
     path('', views.index, name='index'),
+    # Topics pages
+    path('topics/', views.topics, name='topics'),
+    # Detail topic page
+    path('topics/<int:topic_id>/', views.topic, name='topic')
 ]
