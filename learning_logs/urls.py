38353ecfa8d971e48e9cf8cb8a32818752541274
Zakhar Schebet <lovemebaby453@gmail.com>
2020-08-27 19:02:27 +0300

    Add two user forms

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -11,5 +11,9 @@ urlpatterns = [
     # Topics pages
     path('topics/', views.topics, name='topics'),
     # Detail topic page
-    path('topics/<int:topic_id>/', views.topic, name='topic')
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    # New form page
+    path('new_topic/', views.new_topic, name='new_topic'),
+    # New entry page
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
 ]
